6650625f20df6460ce2d1515e50175f8a5da67d0
Mark Henwood <mcbhenwood@icloud.com>
2020-07-17 16:08:03 +0100

    Add Flake8 and lint / tidy the code

--- dhfb/app.py
+++ dhfb/app.py
@@ -2,17 +2,22 @@ import os
 import time
 
 import boto3
-
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, make_response, flash, Response
-
-from flask_env import MetaFlaskEnv
-
 from authbroker_client import authbroker_blueprint, login_required
+from flask import (
+    Flask,
+    Response,
+    g,
+    make_response,
+    redirect,
+    render_template,
+    url_for,
+)
+from flask_env import MetaFlaskEnv
 
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
 
 class Configuration(metaclass=MetaFlaskEnv):
     ENV_PREFIX = 'DHFB_'
@@ -27,14 +32,18 @@ class Configuration(metaclass=MetaFlaskEnv):
     ABC_AUTHORIZE_URL = ''
     ABC_LOGOUT_URL = ''
 
+
 app.config.from_object(Configuration)
 
 app.register_blueprint(authbroker_blueprint)
 
 
 def get_s3_client():
-    """Opens a new database connection if there is none yet for the
-    current application context.
+    """
+    Provide an S3 client.
+
+    Opens a new database connection if there is none yet
+    for the current application context.
     """
     def _get_s3_client():
         s3_client = boto3.client(
@@ -52,7 +61,8 @@ def get_s3_client():
 
 def _ls(path):
     """
-    renders list of files in a given path (namespace)
+    Renders list of files in a given path (namespace).
+
     :param path:
     :return: response object of rendered page
     """
@@ -65,7 +75,10 @@ def _ls(path):
         original_path = path
         path += '/'
 
-    s3_response = s3.list_objects(Bucket=app.config['S3_BUCKET_NAME'], Prefix=path, Delimiter='/')
+    s3_response = s3.list_objects(
+        Bucket=app.config['S3_BUCKET_NAME'],
+        Prefix=path, Delimiter='/',
+    )
 
     if 'Contents' in s3_response:
         ls_dict = map(lambda x: {
@@ -80,9 +93,12 @@ def _ls(path):
 
 def _get(path):
     """
-    attempts to get a file from s3 and stream it
-    no file is locally stored
-    if file doesn't exist (NoSuchKey) then falls back to listing objects from namespace/dir
+    Attempts to get a file from s3 and stream it.
+
+    No file is locally stored. If the file doesn't exist
+    (NoSuchKey) then this falls back to listing objects
+    from namespace/dir.
+
     :param path:
     :return: http response object
     """
@@ -91,16 +107,26 @@ def _get(path):
     try:
         s3_response = s3.get_object(Bucket=app.config['S3_BUCKET_NAME'],
                                     Key=path)
-    except s3.exceptions.NoSuchKey as e:
+    except s3.exceptions.NoSuchKey:
         return _ls(path)
 
     def generate_file(result):
-        for chunk in iter(lambda: result['Body'].read(int(app.config['CHUNK_SIZE'])), b''):
+        def read_chunk():
+            return result['Body'].read(int(app.config['CHUNK_SIZE']))
+
+        for chunk in iter(read_chunk, b''):
             yield chunk
 
     _, filename = os.path.split(path)
-    response = Response(generate_file(s3_response), mimetype=s3_response['ContentType'])
-    response.headers.add('Content-Disposition', 'attachment', filename=filename)
+    response = Response(
+        generate_file(s3_response),
+        mimetype=s3_response['ContentType'],
+    )
+    response.headers.add(
+        'Content-Disposition',
+        'attachment',
+        filename=filename,
+    )
     return response
 
 
@@ -128,7 +154,11 @@ def check():
 
     try:
         s3 = get_s3_client()
-        s3_response = s3.list_objects(Bucket=app.config['S3_BUCKET_NAME'], Prefix='', Delimiter='')
+        s3_response = s3.list_objects(
+            Bucket=app.config['S3_BUCKET_NAME'],
+            Prefix='',
+            Delimiter='',
+        )
         if 'Contents' in s3_response:
             status = 'OK'
         else:
@@ -137,7 +167,11 @@ def check():
         status = 'S3 error: {}'.format(e)
 
     totaltime = time.perf_counter() - start_time
-    rendered = render_template('pingdom.xml', status=status, totaltime=totaltime)
+    rendered = render_template(
+        'pingdom.xml',
+        status=status,
+        totaltime=totaltime,
+    )
     response = make_response(rendered)
     response.headers["Content-Type"] = "application/xml"
     return response
