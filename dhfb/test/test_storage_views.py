6f906d09994e9a83c2a2bd038bf5bc9bf771286b
Mark Henwood <mcbhenwood@icloud.com>
2020-07-22 15:08:07 +0100

    Test that an attachment downloads correctly
    This test requires the S3 bucket to be hooked up. Later tests will
    remove this dependency.

--- dhfb/test/test_storage_views.py
+++ dhfb/test/test_storage_views.py
@@ -31,3 +31,29 @@ def test_list_view_renders(client, monkeypatch, url, listed_filename):
     assert response.status_code == 200
     assert b'These are documents moved from CDMS' in response.data
     assert listed_filename in response.data
+
+
+def test_content_object_attachment_downloads(client, monkeypatch):
+    """
+    Test that a file downloads correctly.
+
+    Currently this test is tightly coupled to the actual
+    contents of the Production S3 bucket. Less assumptive
+    tests will replace these shortly.  # TODO
+    """
+    monkeypatch.setattr(
+        'authbroker_client.is_authenticated',
+        lambda: True,
+    )
+    response = client.get('/storage/kbarticle/Forms/AllItems.aspx')
+
+    assert response.status_code == 200
+    assert len(response.data) == 3370
+    for header, value in (
+            ('Content-Type', 'application/xml; charset=utf-8'),
+            ('Content-Disposition', 'attachment; filename=AllItems.aspx'),
+    ):
+        assert response.headers[header] == value, f'{header} != {value}'
+
+
+# TODO: Set the 'authenticated' status as a pytest fixture
